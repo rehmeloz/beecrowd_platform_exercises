2d8a0295bc2b279950838347b8a2c4b238a66d3b
Renata Melo <renatamelo.pro@outlook.com>
2024-08-29 16:01:38 -0300

    2006

--- Program.cs
+++ Program.cs
@@ -1118,3 +1118,30 @@ for(int i = 0; i < numeroDeCasos; i++)
 
 Console.WriteLine("Ciencia da Computacao");
 */
+
+/*
+// 2006
+int tipoDoCha = int.Parse(Console.ReadLine());
+
+string[] participantes = Console.ReadLine().Split(" ");
+int A = int.Parse(participantes[0]);
+int B = int.Parse(participantes[1]);
+int C = int.Parse(participantes[2]);
+int D = int.Parse(participantes[3]);
+int E = int.Parse(participantes[4]);
+
+int acertos = 0;
+
+if(A == tipoDoCha)
+acertos++;
+if(B == tipoDoCha)
+acertos++;
+if(C == tipoDoCha)
+acertos++;
+if(D == tipoDoCha)
+acertos++;
+if(E == tipoDoCha)
+acertos++;
+
+Console.WriteLine(acertos);
+*/
