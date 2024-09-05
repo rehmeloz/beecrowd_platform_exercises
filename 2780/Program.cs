b030d219a684a44294a8deb0c9d2c5c753c278a7
Renata Melo <renatamelo.pro@outlook.com>
2024-09-05 20:42:45 -0300

    2780

---
+++ 2780/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2780
+int distancia = int.Parse(Console.ReadLine());
+
+if(distancia <= 800)
+{
+    Console.WriteLine("1");
+} else if(distancia > 800 && distancia <= 1400)
+{
+    Console.WriteLine("2");
+} else if(distancia > 1400 && distancia <= 2000)
+{
+    Console.WriteLine("3");
+}
--- obj/Debug/net8.0/Exercicios.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exercicios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98737d2bd7a00bff227a65fb6eaf383be8692b7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2242d521921b5bd263d4fef874bd86638fc4e2e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
